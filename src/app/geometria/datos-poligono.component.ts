3241a1f81625bcba4e518400930f9838c9848a74
LinkSlim <jesusmmz17@hotmail.com>
2021-08-23 14:50:41 +0200

    Los poligonos se ordenan en la tabla por tamaños de menor a mayor

--- src/app/geometria/datos-poligono.component.ts
+++ src/app/geometria/datos-poligono.component.ts
@@ -6,72 +6,89 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./datos-poligono.component.css']
 })
 export class DatosPoligonoComponent implements OnInit {
-  poligonos = [{
-    nombre: 'cuadrado',
-    puntos: [
-      {
-        x: 10,
-        y: 5
-      },
-      {
-        x: 15,
-        y: 5
-      },
-      {
-        x: 15,
-        y: 10
-      },
-      {
-        x: 10,
-        y: 10
-      }
-    ]
-  },
-  {
-    nombre: 'triangulo',
-    puntos: [
-      {
-        x: 10,
-        y: 5
-      },
-      {
-        x: 15,
-        y: 5
-      },
-      {
-        x: 15,
-        y: 10
-      }
-    ]
-  },
-  {
-    nombre: 'pentagono',
-    puntos: [
-      {
-        x: 10,
-        y: 5
-      },
-      {
-        x: 15,
-        y: 5
-      },
-      {
-        x: 15,
-        y: 10
-      },
-      {
-        x: 15,
-        y: 10
-      },
-      {
-        x: 15,
-        y: 10
-      }
-    ]
-  }];
+  poligonos = [
+    {
+      nombre: 'cuadrado',
+      puntos: [
+        {
+          x: 10,
+          y: 5
+        },
+        {
+          x: 15,
+          y: 5
+        },
+        {
+          x: 15,
+          y: 10
+        },
+        {
+          x: 10,
+          y: 10
+        }
+      ]
+    },
+    {
+      nombre: 'triangulo',
+      puntos: [
+        {
+          x: 10,
+          y: 5
+        },
+        {
+          x: 15,
+          y: 5
+        },
+        {
+          x: 15,
+          y: 10
+        }
+      ]
+    },
+    {
+      nombre: 'pentagono',
+      puntos: [
+        {
+          x: 10,
+          y: 5
+        },
+        {
+          x: 15,
+          y: 5
+        },
+        {
+          x: 15,
+          y: 10
+        },
+        {
+          x: 15,
+          y: 10
+        },
+        {
+          x: 15,
+          y: 10
+        }
+      ]
+    }
+  ];
 
- //mayorNumeroPuntos = 5;
-  mayorNumeroPuntos = Math.max.apply(Math, this.poligonos.map(function(o) { return o.puntos.length; }))
+  //mayorNumeroPuntos = 5;
+  mayorNumeroPuntos = Math.max.apply(
+    Math,
+    this.poligonos.map(function(o) {
+      return o.puntos.length;
+    })
+  );
+
+  sortByLength() {
+    return this.poligonos.sort((a, b) =>
+      a.puntos.length > b.puntos.length
+        ? 1
+        : a.puntos.length === b.puntos.length
+        ? 0
+        : -1
+    );
+  }
 
   constructor() {}
 
